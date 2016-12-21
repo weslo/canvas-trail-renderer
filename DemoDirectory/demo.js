0ce17b4f6371c410d798c2aa51a40515b2e342d5
Weslo <wesley.rockholz@gmail.com>
2016-12-21 14:12:07 -0500

    Render a line as the ball is dragged around

--- DemoDirectory/demo.js
+++ DemoDirectory/demo.js
@@ -7,24 +7,64 @@ var ball;
 var Ball = function() {
   this.pos = new Point(canvas.width / 2, canvas.height / 2);
   this.line = Array;
+
+  this.trail = new TrailRenderer(1, 5);
+
+  this.render = function(ctx) {
+    this.trail.render(ctx);
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, 10, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 };
 
 var Point = function(x, y) {
   this.x = x;
   this.y = y;
+
+  this.distance = function(other) {
+    return Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2));
+  }
+};
+
+var TrailRenderer = function(width, time, minVertexDistance = 0.1) {
+  this.width = width;
+  this.time = time;
+  this.minVertexDistance = minVertexDistance;
+
+  this.vertices = [];
+
+  this.update = function(pos) {
+    var last = this.vertices[0];
+    var first = this.vertices[this.vertices.length - 1];
+
+    if(last == null || last == 'undefined' || last.distance(pos) > this.minVertexDistance) {
+      this.vertices.push(new Point(pos.x, pos.y));
+    }
+  }
+
+  this.render = function(ctx) {
+    ctx.beginPath();
+    if(this.vertices.length > 0) {
+      ctx.moveTo(this.vertices[0].x, this.vertices[0].y);
+      this.vertices.map(function(vert, index) {
+        ctx.lineTo(vert.x, vert.y);
+      });
+    }
+    ctx.stroke();
+  }
 };
 
 function redraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.arc(ball.pos.x, ball.pos.y, 10, 0, 2 * Math.PI);
-  ctx.fill();
+  ball.render(ctx);
 }
 
 function mousemove(e) {
   if(dragging) {
     ball.pos.x = e.offsetX;
     ball.pos.y = e.offsetY;
+    ball.trail.update(ball.pos);
     redraw();
   }
 }
